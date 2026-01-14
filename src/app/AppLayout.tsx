9ed8399139dec0aea48dcbfb23f10d187af1d93c
Nikhil <nikhilshakohi@gmail.com>
2026-01-14 22:57:23 +0530

    Added sidebar

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,18 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
+import Sidebar from "../components/Sidebar";
 
 export default function AppLayout() {
   return (
     <div className="app-layout">
-      {/* Header */}
       <Header />
 
       <div className="app-body">
-        {/* Sidebar */}
-        <aside>Sidebar</aside>
+        <Sidebar />
 
-        {/* Main content */}
-        <main>
+        <main className="app-main">
           <Outlet />
         </main>
       </div>
